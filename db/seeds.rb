65c2b1c3f310169f87c1458a1e4862ffb0036cfe
SamHarron <harronsam@gmail.com>
2021-06-11 23:31:58 -0400

    commit tree 10bb06b1be52eec586ceb440a7eec6360dde4eea

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+roanokecounty = Student.create ([{first_name: 'Barry', last_name: 'Bonds'}])
